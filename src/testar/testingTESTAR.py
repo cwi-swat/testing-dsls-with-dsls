98c22031b500da2cc5bda7e6f3b3cd83af3e443a
Tanja Vos <tvos@pros.upv.es>
2024-08-21 19:01:16 +0200

    Testing miniTESTAR with parabank

--- src/testar/testingTESTAR.py
+++ src/testar/testingTESTAR.py
@@ -1,36 +1,13 @@
-from miniTESTAR import testar
+from miniTESTAR import testar, start_SUT_and_get_driver
 from selenium.webdriver.common.by import By
 import os
 
 
 def test_lambda():
     
-    testar('https://lambdatest.github.io/sample-todo-app/', 3, 100, [], [], [])
+    testar('https://lambdatest.github.io/sample-todo-app/', 3, 100, [], [], [], 20)
 
 
-def test_tax():
-      
-    testar('file://' + os.getcwd() + '/../../examples/tax.html', 1, 500, [], [], [])
-
-def test_binary():
-    
-    testar('file://' + os.getcwd() + '/../../examples/binary.html', 3, 100, [], [], [])
-
-
-def test_cyclic():
-    
-    testar('file://' + os.getcwd() + '/../../examples/cyclic.html', 3, 100, [], [], [])
-
-
-def test_demo():
-    
-    testar('file://' + os.getcwd() + '/../../examples/demo.html', 3, 100, [], [], [])
-    
-def test_loan_approval():
-    
-    testar('file://' + os.getcwd() + '/../../examples/loan_approval.html', 3, 100, [], [], [])
-
-  
 def test_saucer():
   
     def test_Sauce_login(driver):
@@ -55,7 +32,41 @@ def test_saucer():
         
         return social_links
     
-    testar("https://www.saucedemo.com/", 1, 100, [filter_burger, filter_external_links], [test_Sauce_login], [])
+    testar("https://www.saucedemo.com/", 1, 100, [filter_burger, filter_external_links], [test_Sauce_login], [], 20)
      
     
+def test_parabank():
     
+    def filter_external_links(driver):
+        # Find all anchors
+        all_links = driver.find_elements(By.TAG_NAME, "a")
+        
+        # Filter the links to parasoft.com
+        unwanted = ["parasoft.com","?_wadl&_type=xml","?wsdl"]
+        external_links = [ link for link in all_links if link.get_attribute("href") and any(href in link.get_attribute("href") for href in unwanted)]
+        
+        return external_links
+    
+    def filter_zero_sized_elements(driver):
+        # Find all elements on the page
+        all_elements = driver.find_elements(By.XPATH, "//*")
+        
+        # Filter elements that have size {'height': 0, 'width': 0}
+        zero_sized_elements = [element for element in all_elements if element.size['height'] == 0 and element.size['width'] == 0]
+        
+        return zero_sized_elements
+        
+    def parabank_login(driver):
+       
+        # Localizar username y escibir uno
+        driver.find_element(By.NAME, "username").send_keys("john")
+        
+        # Localizar password y escibir uno
+        driver.find_element(By.NAME, "password").send_keys("demo")
+        
+        # Localizar el botón y hacer click
+        driver.find_element(By.XPATH, "//input[@type='submit' and @value='Log In']").click()
+
+    
+    testar("https://para.testar.org/", 1, 40, [filter_external_links, filter_zero_sized_elements], [parabank_login], [], 180)
+
